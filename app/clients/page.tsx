6bfcebbbe590d1a0cb5b1cf05dc22b87e6513ed2
MokuKen <imsilencer44l@gmail.com>
2026-06-04 22:19:59 +0800

    feat: remove unused Link import in ClientsPage

--- app/clients/page.tsx
+++ app/clients/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState, useEffect, useRef } from "react";
-import Link from "next/link";
 import Image from "next/image";
 import Sidebar from "@/components/Sidebar";
 import Header from "@/components/Header";
